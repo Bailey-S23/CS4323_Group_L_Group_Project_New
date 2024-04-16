3f45420d6996fdda068b6a574478581cc4d7a209
Katie Pinell <katiepinell@Katies-MacBook-Air.local>
2024-04-16 18:19:01 -0500

    Solve conflict

--- CloseAccount.h
+++ CloseAccount.h
@@ -11,11 +11,8 @@ class CloseAccount
 
     public:
         string accName;
-<<<<<<< HEAD
         CloseAccount(const vector<string>& accountInfo);
-=======
         CloseAccount(vector<string> accountInfo);
->>>>>>> 2f355eaa14f8c7b1c6e2af70a4b0e7fd086970bb
 };
 
 #endif
