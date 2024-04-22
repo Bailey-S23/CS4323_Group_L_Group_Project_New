9b599331d9de2eb3e2257c3838472610729fa7b7
bailey <bailey.schultz@okstate.edu>
2024-04-21 20:28:44 -0500

    small updates

--- CreateAccount.cpp
+++ CreateAccount.cpp
@@ -14,8 +14,6 @@
 #include <sstream>
 #include <iomanip>
 
-
-
 bool CreateAccount::accExists(string accountNumber)
 {
 
@@ -27,28 +25,25 @@ bool CreateAccount::accExists(string accountNumber)
 
     if (file)
     {
-        cout << "File " + accountNumber + " exists" << endl;
         exists = true;
     }
     else
     {
-        cout << "File " + accountNumber + " does not exist" << endl;
-
         exists = false;
     }
 
     return exists;
 }
-
+/************************************* BELOW THIS LINE ADDED FOR SHARED MEMORY *************************************/
 string CreateAccount::returnCurrentTimeAndDate()
 {
     auto currentTime = chrono::system_clock::now();
     auto currentTime_t = chrono::system_clock::to_time_t(currentTime);
-
     stringstream sstream;
     sstream << std::put_time(localtime(&currentTime_t), "%Y-%m-%d %X");
     return sstream.str();
 }
+/************************************* ABOVE THIS LINE ADDED FOR SHARED MEMORY *************************************/
 
 // Constructor that creates new account files
 CreateAccount::CreateAccount(vector<string> accountInfo, void *sharedMemory)
@@ -59,35 +54,40 @@ CreateAccount::CreateAccount(vector<string> accountInfo, void *sharedMemory)
     if (accBalance >= 0)
     {
 
-    string temp = "Accounts/" + accountInfo[0];
+        string temp = "Accounts/" + accountInfo[0];
 
-    struct stat sb;
+        struct stat sb;
 
-    ofstream outfile("Accounts/" + accName);
-    outfile << accBalance;
+        ofstream outfile("Accounts/" + accName);
+        outfile << accBalance;
 
-    outfile.close();
-
+        outfile.close();
     }
 
     //------------------------------------------
 
     if (accExists(accountInfo[0]))
     {
+
+        cout << "Account " + accName + " Created" << endl;
+
+        /************************************* BELOW THIS LINE ADDED FOR SHARED MEMORY *************************************/
         string time = returnCurrentTimeAndDate();
         string writeToFile = "Transaction type: Create " + accName + " " + accountInfo[2] + " SUCCESS " + time + "\n";
         char *writeInLog = writeToFile.data();
         strcat((char *)sharedMemory, writeInLog);
+        /************************************* ABOVE THIS LINE ADDED FOR SHARED MEMORY *************************************/
     }
     else
     {
+        cout << "Account " + accName + " Failed to be Created" << endl;
+
+        /************************************* BELOW THIS LINE ADDED FOR SHARED MEMORY *************************************/
         string time = returnCurrentTimeAndDate();
-        string writeToFile = "Transaction type: Create " + accName + " " + accountInfo[2] + " FAILURE " + time+ "\n";
-
+        string writeToFile = "Transaction type: Create " + accName + " " + accountInfo[2] + " FAILURE " + time + "\n";
         char *readInCreate = (char *)sharedMemory;
-
         char *writeInLog = writeToFile.data();
-
         strcat((char *)sharedMemory, writeInLog);
+        /************************************* ABOVE THIS LINE ADDED FOR SHARED MEMORY *************************************/
     }
 }
